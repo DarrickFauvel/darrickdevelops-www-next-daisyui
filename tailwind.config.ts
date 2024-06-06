aa18053740870aea329ff4c94ea4428c85caf9d6
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2024-06-06 17:20:26 -0400

    Add hand wave animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,6 +16,24 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      animation: {
+        "hand-wave": "hand-wave 2.5s ease-in-out infinite",
+      },
+      keyframes: {
+        "hand-wave": {
+          "0%": {
+            transform: "rotate(0)",
+            "transform-origin": "70% 70%",
+          },
+          "10%": { transform: "rotate(14.0deg)" },
+          "20%": { transform: "rotate(-8.0deg)" },
+          "30%": { transform: "rotate(14.0deg)" },
+          "40%": { transform: "rotate(-4.0deg)" },
+          "50%": { transform: "rotate(10.0deg)" },
+          "60%": { transform: "rotate( 0.0deg)" },
+          "100%": { transform: "rotate( 0.0deg)" },
+        },
+      },
     },
   },
   daisyui: {
